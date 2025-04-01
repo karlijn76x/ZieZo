949a1cbf76312d6d8a8ea8189d96e221c74ff8b7
karlijn76x <karlijnvandenherik@gmail.com>
2025-04-01 09:50:52 +0200

    Initial Commit

---
+++ ZieZo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZieZo
+//
+//  Created by Karlijn van den Herik on 01/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZieZo/ZieZoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZieZoApp.swift
+//  ZieZo
+//
+//  Created by Karlijn van den Herik on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZieZoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
